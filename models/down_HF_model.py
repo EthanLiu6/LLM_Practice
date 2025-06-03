18b5c8b00280ca6e2f016f111c7d4da3ab3cdb5f
EthanLiu6 <liue2336@gmail.com>
2025-06-03 21:43:52 +0800

    add model download

---
+++ models/down_HF_model.py
@@ -0,0 +1,10 @@
+from huggingface_hub import snapshot_download
+
+def download_model(model_name: str, local_dir: str = "./"):
+    snapshot_download(repo_id=model_name, local_dir=local_dir)
+
+if __name__ == "__main__":
+    model_name = "Qwen/Qwen3-0.6B"
+    local_dir = "./"
+    download_model(model_name, local_dir)
+    print(f"Model {model_name} downloaded to {local_dir}")
