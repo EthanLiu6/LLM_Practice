7884e6b2aa6c93876003ac74c80b557d9e91217a
EthanLiu6 <liue2336@gmail.com>
2025-06-04 01:22:48 +0800

    fix merge_model.py

--- 微调与应用/merge_model.py
+++ 微调与应用/merge_model.py
@@ -1,10 +1,11 @@
-from peft import LoraConfig, get_peft_model
-
-
+from peft import LoraConfig, TaskType, PeftModel
+from transformers import AutoModelForCausalLM
+from transformers import AutoTokenizer
 from lora_qwen3_config import *
 
-# 加载原始模型
+# 1. 加载原始模型
 base_model = AutoModelForCausalLM.from_pretrained(qwen3_model_path)
+tokenizer = AutoTokenizer.from_pretrained(qwen3_model_path)
 
 # 初始化与训练时相同的 LoRA 配置
 lora_config = LoraConfig(
@@ -12,13 +13,12 @@ lora_config = LoraConfig(
     inference_mode=False,
     target_modules=["q_proj", "k_proj", "v_proj", "o_proj"])
 
-# 绑定 LoRA 到原始模型
-qwen3_lora_model = get_peft_model(base_model, peft_config)
+# 2. 加载 LoRA Adapter
+lora_model = PeftModel.from_pretrained(base_model, lora_adapter_path)
 
-# 加载训练好的 LoRA 权重
-qwen3_lora_model.load_state_dict(torch.load(lora_adapter_path))
+# 3. 合并 LoRA 权重
+merged_model = lora_model.merge_and_unload()
 
-# 合并并保存
-merged_model = model.merge_and_unload()
+# 4. 保存合并后的模型
 merged_model.save_pretrained(merged_model_path, max_shard_size="3GB")
-tokenizer.save_pretrained(merged_model_path)  # 同时保存分词器
+tokenizer.save_pretrained(merged_model_path)
