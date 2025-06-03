19fb6484cb127fee564b4d6c3821fbe464cd5ba7
EthanLiu6 <liue2336@gmail.com>
2025-06-03 21:50:41 +0800

    update download model

--- models/down_HF_model.py
+++ models/down_HF_model.py
@@ -1,10 +1,12 @@
 from huggingface_hub import snapshot_download
 
-def download_model(model_name: str, local_dir: str = "./"):
+
+def download_model(model_name: str, local_dir: str = "./xxx"):
     snapshot_download(repo_id=model_name, local_dir=local_dir)
+
 
 if __name__ == "__main__":
     model_name = "Qwen/Qwen3-0.6B"
-    local_dir = "./"
+    local_dir = "./Qwen/Qwen3-0.6B"
     download_model(model_name, local_dir)
     print(f"Model {model_name} downloaded to {local_dir}")
