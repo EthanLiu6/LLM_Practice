222515c25db5677122b8f37e39b9abd871134bcc
EthanLiu6 <liue2336@gmail.com>
2025-06-03 21:47:46 +0800

    update fine-tuning config

--- 微调与应用/lora_qwen3_config.py
+++ 微调与应用/lora_qwen3_config.py
@@ -6,6 +6,6 @@ json_file = "./data/sighan_2015/train.json"
 output_dir = './out_lora_train'
 
 # merge
-qwen3_model_path = '/mnt/workspace/model_list/Qwen/Qwen3-8B'
+# qwen3_model_path = '/mnt/workspace/model_list/Qwen/Qwen3-8B'
 lora_adapter_path = './lora_models/lora_qwen3-8B—TextCorrection'
 merged_model_path = './merged_model'
